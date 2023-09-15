f97b456695d40f193bc001af851e684ed7b9b944
Keenan Shumard <keenan.shumard@gmail.com>
2023-09-14 19:30:46 -0500

    Adding movies object

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "movies#index"
+  get "/movies", to: "movies#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
